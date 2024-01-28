b0c1c7f16cba7db7c1ea633c3cca7a81e92ab63f
John Mutua <mutubenedict920@gmail.com>
2024-01-28 10:58:06 +0300

    Added slider app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import styles from "./style.module.css"
+import { FaQuoteRight } from "react-icons/fa6";
+import { FaChevronLeft } from "react-icons/fa";
+import { FaChevronRight } from "react-icons/fa";
 
+import Data from "./data"
 function App() {
+  const[people,setPeople] = useState(Data)
+  const[index,setIndex] = useState(0)
+  useEffect(()=>{
+    let lastIndex = people.length - 1
+    if (index < 0){
+      setIndex(lastIndex)
+    }
+    if(index > lastIndex){
+      setIndex(0)
+    }
+  },[index,people])
+  useEffect(()=>{
+    let slider = setInterval(()=>{
+      setIndex(index + 1)
+    },3000)
+    return () => {
+       clearInterval(slider)
+    }
+  },[index]) 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className={styles.section}>
+      <div className={styles.title}>
+        <h2>
+          <span>/</span> Reviews
+        </h2>
+      </div>
+      <div className={styles.sectioncenter}>
+        {people.map((person,personIndex) => {
+          const{id,image,name,title,quote} = person;
+          let position = styles.nextSlide
+          if(personIndex === index){
+            position = styles.activeSlide
+          }
+          if(personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)){
+            position = styles.lastSlide
+          }
+          return (<article key = {id} className={`${position}`}>
+            <img src={image} alt={name} className={styles.personimg} />
+            <h4>{name}</h4>
+            <p className={styles.title}>{title}</p>
+            <p className={styles.text}>{quote}</p>
+            <FaQuoteRight className={styles.icon} />
+          </article>)
+        })}
+        <button className={styles.prev} onClick={() =>{setIndex(index - 1)}}>
+          <FaChevronLeft />
+        </button>
+        <button className={styles.next} onClick={() => {setIndex(index + 1)}}>
+          <FaChevronRight />
+          </button>
+      </div>
+    </section>
+    
   );
 }
 
